a0545aa594cdd05f673e6edc31d3671283ea14b9
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-10-31 22:11:53 -0500

    Rename app

--- fastapi_demo/main.py
+++ fastapi_demo/main.py
@@ -10,19 +10,19 @@ from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-class Idea(BaseModel):
-    idea_id: int
-    idea_name: str
-    idea_description: Optional[str] = None
-    idea_detail: Optional[str] = None
-    idea_creation_date: str
+class item(BaseModel):
+    item_id: int
+    item_name: str
+    item_description: Optional[str] = None
+    item_detail: Optional[str] = None
+    item_creation_date: str
     user: str
 
 class User(BaseModel):
     username: str
     email: Optional[str] = None
 
-billow = FastAPI()
+app = FastAPI()
 
 # Dependency
 def get_db():
@@ -32,17 +32,17 @@ def get_db():
     finally:
         db.close()
 
-@billow.post("/ideas/")
-async def create_item(ideas: Idea):
-    return idea
+@app.post("/items/")
+async def create_item(items: item):
+    return item
 
-@billow.put("/ideas/{idea_id}")
-async def create_item(idea_id: int, idea: Idea):
-    return {"idea_id": idea_id, **idea.dict()}
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: item):
+    return {"item_id": item_id, **item.dict()}
 
-@billow.get("/ideas/")
-async def read_ideas(q: Optional[str] = None):
-    results = {"ideas": [{"idea_id": "Foo"}, {"idea_id": "Bar"}]}
+@app.get("/items/")
+async def read_items(q: Optional[str] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
     return results
